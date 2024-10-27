8d284fb002d4248fb32f40c49e6c19531b8d1a37
Sasha Borisov <sb.2004.bor@gmail.com>
2024-10-27 20:55:35 +0300

    add functionality for space shooter(with no bugs, but hard control)

--- lib/screens/space_shooter.dart
+++ lib/screens/space_shooter.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class SpaceShooterScreen extends StatefulWidget {
@@ -7,21 +9,145 @@ class SpaceShooterScreen extends StatefulWidget {
 
 class _SpaceShooterScreenState extends State<SpaceShooterScreen> {
   double spaceshipX = 0;
+  double targetX = 0;
+  List<Map<String, double>> bullets = [];
+  List<Map<String, double>> enemies = [];
+  bool isGameRunning = false;
+  Timer? gameTimer;
+  int score = 0;
 
-  void _moveLeft() {
+  void _startGame() {
     setState(() {
-      spaceshipX -= 0.1;
+      isGameRunning = true;
+      spaceshipX = 0;
+      targetX = 0;
+      bullets = [];
+      enemies = [];
+      score = 0;
+    });
+
+    gameTimer = Timer.periodic(Duration(milliseconds: 16), (timer) {
+      setState(() {
+        bullets = bullets.map((bullet) {
+          return {'x': bullet['x']!, 'y': bullet['y']! + 0.02};
+        }).toList();
+        bullets.removeWhere((bullet) => bullet['y']! > 1);
+
+        for (var enemy in enemies) {
+          enemy['y'] = enemy['y']! + enemy['speed']!;
+          if (enemy['y']! > 1) {
+            _gameOver();
+          }
+        }
+        enemies.removeWhere((enemy) => enemy['y']! > 1);
+
+        for (int i = bullets.length - 1; i >= 0; i--) {
+          for (int j = enemies.length - 1; j >= 0; j--) {
+            if ((bullets[i]['x']! - enemies[j]['x']!).abs() < 0.05 &&
+                (bullets[i]['y']! - enemies[j]['y']!).abs() < 0.05) {
+              enemies.removeAt(j);
+              bullets.removeAt(i);
+              score++;
+              break;
+            }
+          }
+        }
+
+        if ((spaceshipX - targetX).abs() > 0.01) {
+          spaceshipX += (targetX - spaceshipX) * 0.1;
+        }
+      });
+    });
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (isGameRunning) {
+        setState(() {
+          bullets.add({'x': spaceshipX, 'y': 0});
+        });
+      }
+    });
+
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      if (isGameRunning) {
+        setState(() {
+          enemies.add({
+            'x': Random().nextDouble() * 2 - 1,
+            'y': -1.0,
+            'speed': Random().nextDouble() * 0.01 + 0.005
+          });
+        });
+      }
     });
   }
 
-  void _moveRight() {
+  void _gameOver() {
+    gameTimer?.cancel();
     setState(() {
-      spaceshipX += 0.1;
+      isGameRunning = false;
+    });
+  }
+
+  void _updateTargetX(double cursorX) {
+    setState(() {
+      targetX = (cursorX / MediaQuery.of(context).size.width) * 2 - 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTapDown: (_) {
+        if (!isGameRunning) {
+          _startGame();
+        }
+      },
+      onPanUpdate: (details) {
+        _updateTargetX(details.localPosition.dx);
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: 50,
+              left: MediaQuery.of(context).size.width * (0.5 + spaceshipX) - 25,
+              child: Container(
+                width: 50,
+                height: 50,
+                color: Colors.grey,
+              ),
+            ),
+            for (var bullet in bullets)
+              Positioned(
+                bottom: MediaQuery.of(context).size.height * bullet['y']!,
+                left: MediaQuery.of(context).size.width * (0.5 + bullet['x']!) -
+                    5,
+                child: Container(
+                  width: 10,
+                  height: 20,
+                  color: Colors.red,
+                ),
+              ),
+            for (var enemy in enemies)
+              Positioned(
+                top: MediaQuery.of(context).size.height * enemy['y']!,
+                left: MediaQuery.of(context).size.width * (0.5 + enemy['x']!),
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.green,
+                ),
+              ),
+            Positioned(
+              top: 30,
+              left: 10,
+              child: Text(
+                'Очки: $score',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
